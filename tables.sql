e9e00444214df77ed956abb182a4dd708ae06a62
Rickens Walter Otieno <rickensotieno@gmail.com>
2025-04-10 22:07:48 +0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,1 +1,16 @@
+ -- Create database
+CREATE DATABASE IF NOT EXISTS bookstore_data;
+Use bookstore_data;
 
+-- Create Tables
+CREATE TABLE book(
+  book_id INT PRIMARY KEY AUTOINCREMENT,
+  book_name VARCHAR(50) NOT NULL);
+
+CREATE TABLE book_author(
+  book_id INT,
+  author_id INT PRIMARY KEY AUTOINCREMENT,
+  author VARCHAR(100) NOT NULL);
+
+
+  

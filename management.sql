15c2e153a52e30111a42516dc9a3b14eac9dad88
Rickens Walter Otieno <rickensotieno@gmail.com>
2025-04-13 10:04:47 +0300

    Update management.sql

--- management.sql
+++ management.sql
@@ -1,17 +1,17 @@
-CREATE USER 'book_admin'@'localhost' IDENTIFIED BY 'StrongPassword123!';
-CREATE USER 'sales_analyst'@'localhost' IDENTIFIED BY 'ReadOnly456!';
-CREATE USER 'shipping_clerk'@'localhost' IDENTIFIED BY 'ShipIt789!';
+-- Creating users
+CREATE USER 'book_admin'@'localhost' IDENTIFIED BY 'Snowflake123!';
+CREATE USER 'sales_analyst'@'localhost' IDENTIFIED BY 'salesrep123!';
+CREATE USER 'shipping_rep'@'localhost' IDENTIFIED BY 'orderanalyst123!';
 
+-- database admin
 GRANT ALL PRIVILEGES ON bookstore_data.* TO 'book_admin'@'localhost';
 
+-- salesanalyst
 GRANT SELECT ON bookstore_data.customer TO 'sales_analyst'@'localhost';
 GRANT SELECT ON bookstore_data.cust_order TO 'sales_analyst'@'localhost';
 GRANT SELECT ON bookstore_data.order_line TO 'sales_analyst'@'localhost';
 
-GRANT SELECT ON bookstore_data.customer TO 'sales_analyst'@'localhost';
-GRANT SELECT ON bookstore_data.cust_order TO 'sales_analyst'@'localhost';
-GRANT SELECT ON bookstore_data.order_line TO 'sales_analyst'@'localhost';
-
+-- shippingrep
 GRANT SELECT, UPDATE ON bookstore_data.order_status TO 'shipping_clerk'@'localhost';
 GRANT SELECT, INSERT ON bookstore_data.order_history TO 'shipping_clerk'@'localhost';
 GRANT SELECT ON bookstore_data.shipping_method TO 'shipping_clerk'@'localhost';
